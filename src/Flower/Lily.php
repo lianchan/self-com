32b77527378a0db49b654ebcc8fc4a5c4f3bb964
shchenjianhua <unnnnn@foxmail.com>
2018-06-15 00:44:07 +0800

    init && dev package

---
+++ src/Flower/Lily.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenjianhua
+ * Date: 2018/6/15
+ * Time: 上午12:42
+ */
+namespace Flower\Lily;
+
+
+class Lily
+{
+    public function desc()
+    {
+        echo "this is lily flower";
+    }
+}
---
+++ src/Flower/Rose.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenjianhua
+ * Date: 2018/6/15
+ * Time: 上午12:42
+ */
+namespace Flower\Rose;
+
+
+class Rose
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
